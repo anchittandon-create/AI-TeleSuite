ae16b087d0a399da3eb78322e52c3c7d21503b22
Anchit Tandon <anchit.tandon@timesinternet.in>
2025-08-25 20:39:35 +0000

    wrong implementation of the change - if user doesnt say anything for 50

--- src/hooks/useWhisper.ts
+++ src/hooks/useWhisper.ts
@@ -31,7 +31,7 @@ export function useWhisper({
   onTranscriptionComplete,
   autoStart = false,
   autoStop = false,
-  stopTimeout = 900, 
+  stopTimeout = 90, 
 }: UseWhisperProps) {
   const [isRecording, setIsRecording] = useState<boolean>(false);
   const [transcript, setTranscript] = useState<Transcript>({ text: '', isFinal: false });
