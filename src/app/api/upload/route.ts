ab852af44d234cd6468a9bab0e39eaf16900388a
AT-2803 <anchit.tandon@timesinternet.in>
2025-10-19 21:44:29 +0530

    changesss

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,22 +1,35 @@
 
-import { put } from '@vercel/blob';
+import { handleUpload, type HandleUploadBody } from '@vercel/blob/server';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request): Promise<NextResponse> {
-  const { searchParams } = new URL(request.url);
-  const filename = searchParams.get('filename');
-
-  if (!filename || !request.body) {
-    return NextResponse.json({ message: 'Missing filename or request body' }, { status: 400 });
-  }
+  const body = (await request.json()) as HandleUploadBody;
 
   try {
-    const blob = await put(filename, request.body, {
-      access: 'public',
+    const jsonResponse = await handleUpload({
+      body,
+      request,
+      onBeforeGenerateToken: async (pathname: string) => {
+        // This callback is called before a token is generated.
+        // It can be used to check for permissions, rate limiting, etc.
+        return {
+          allowedContentTypes: ['audio/mpeg', 'audio/wav', 'audio/x-wav', 'audio/mp3'],
+          tokenPayload: JSON.stringify({
+            // Optional payload to be passed to the onUploadCompleted callback
+          }),
+        };
+      },
+      onUploadCompleted: async ({ blob, tokenPayload }) => {
+        // This callback is called after the file has been uploaded.
+        console.log('blob upload completed', blob, tokenPayload);
+      },
     });
 
-    return NextResponse.json(blob);
-  } catch (error: any) {
-    return NextResponse.json({ message: `Error uploading file: ${error.message}` }, { status: 500 });
+    return NextResponse.json(jsonResponse);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 400 }, // The webhook will retry 5 times waiting for a 200
+    );
   }
 }
